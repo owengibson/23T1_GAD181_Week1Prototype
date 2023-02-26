b33fad4af393a8a307e6f24440ecfaa39534b7f3
owengibson <114120616+owengibson@users.noreply.github.com>
2023-02-26 18:48:54 +1100

    Made walls bouncy
    Still quite buggy, but better than nothing.

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -7,10 +7,37 @@ namespace Chowen
     public class WallCollision : MonoBehaviour
     {
         [SerializeField] private AudioManager audioManager;
+        [SerializeField] private Rigidbody player;
+        [SerializeField] private float bounceStrength = 2f;
+
         private void OnCollisionEnter(Collision collision)
         {
-            //Debug.Log(collision.gameObject);
             audioManager.Play("WallCollision");
+
+            //Vector3 forceDirection = -player.velocity.normalized * bounceStrength;
+            //Debug.Log(forceDirection);
+            //player.AddForce(new Vector3(100, 0, 100));
+
+            //switch (gameObject.name)
+            //{
+            //    case "TL":
+            //        player.AddForce()
+            //        break;
+            //    case "TR":
+
+            //        break;
+            //    case "BL":
+
+            //        break;
+            //    case "BR":
+
+            //        break;
+            //    default:
+            //        break;
+
+
+
+            //}
         }
     }
 }

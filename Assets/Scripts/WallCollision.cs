c611cae9d106ecb9d75b3d489e051c76a9b12e8c
owengibson <114120616+owengibson@users.noreply.github.com>
2023-02-27 11:04:08 +1100

    Improved player bouncing off walls
    Bounciness/control still does not feel quite right, but mechanic is working. Also lowered player movement speed to compensate for velocity gained when bouncing.
    Codecks ID: $125

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -7,37 +7,10 @@ namespace Chowen
     public class WallCollision : MonoBehaviour
     {
         [SerializeField] private AudioManager audioManager;
-        [SerializeField] private Rigidbody player;
-        [SerializeField] private float bounceStrength = 2f;
 
         private void OnCollisionEnter(Collision collision)
         {
             audioManager.Play("WallCollision");
-
-            //Vector3 forceDirection = -player.velocity.normalized * bounceStrength;
-            //Debug.Log(forceDirection);
-            //player.AddForce(new Vector3(100, 0, 100));
-
-            //switch (gameObject.name)
-            //{
-            //    case "TL":
-            //        player.AddForce()
-            //        break;
-            //    case "TR":
-
-            //        break;
-            //    case "BL":
-
-            //        break;
-            //    case "BR":
-
-            //        break;
-            //    default:
-            //        break;
-
-
-
-            //}
         }
     }
 }
